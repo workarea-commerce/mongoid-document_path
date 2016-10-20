e60206059521115f3eea28fd86cc2b5a4ff788fe
Ben Crouse <bcrouse@weblinc.com>
2016-10-20 09:11:46 -0400

    Get root document path working

--- lib/mongoid/document_path.rb
+++ lib/mongoid/document_path.rb
@@ -1,7 +1,59 @@
-require "mongoid/document_path/version"
+require 'mongoid'
+require 'mongoid/document_path/version'
 
 module Mongoid
   module DocumentPath
-    # Your code goes here...
+    class Node
+      include Mongoid::Document
+
+      field :type, type: String
+      field :document_id, type: String
+      field :relation, type: String
+      embedded_in :document_path, polymorphic: true
+    end
+
+    extend ActiveSupport::Concern
+
+    class << self
+      def find(path)
+        return nil if path.blank?
+
+        root = path.first.type.constantize.find(path.first.document_id)
+        return root if path.one?
+
+        path.reduce(root) do |current, path|
+          relation_match = if document_path_matches?(path, current)
+                             current
+                           elsif current.respond_to?(:detect)
+                             current.detect do |model|
+                               document_path_matches?(path, model)
+                             end
+                           end
+
+          if path['relation'].blank?
+            return relation_match
+          else
+            relation_match.send(path['relation'])
+          end
+        end
+      end
+
+      def document_path_matches?(path, object)
+        object.class.name == path['type'] && object.id == path['id']
+      end
+    end
+
+    def document_path(node = self, current_relation = nil)
+      relation = node.embedded? ? node.metadata_name.to_s : nil
+      list = node._parent ? document_path(node._parent, relation) : []
+
+      list << Node.new(
+        type: node.class.name,
+        document_id: node.id,
+        relation: current_relation
+      )
+
+      list
+    end
   end
 end
--- test/mongoid/document_path_test.rb
+++ test/mongoid/document_path_test.rb
@@ -1,11 +1,24 @@
 require 'test_helper'
 
 class Mongoid::DocumentPathTest < Minitest::Test
+  class Order
+    include Mongoid::Document
+    include Mongoid::DocumentPath
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Mongoid::DocumentPath::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_root_object_document_paths
+    order = Order.create!
+    document_path = order.document_path
+
+    assert_equal(document_path.length, 1)
+    assert_equal(document_path.first.type, Order.name)
+    assert_equal(document_path.first.document_id, order.id.to_s)
+    assert_nil(document_path.first.relation)
+
+    assert_equal(Mongoid::DocumentPath.find(document_path), order)
   end
 end
