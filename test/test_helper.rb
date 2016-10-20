1f90d857316910d3913576fa76f297d43c92c7c2
Ben Crouse <bcrouse@weblinc.com>
2016-10-20 09:03:40 -0400

    Set up tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,18 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'mongoid/document_path'
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+Mongoid.configure do |config|
+ config.connect_to('mongoid_audit_log_test')
+end
+
+Minitest::Reporters.use! [
+ Minitest::Reporters::DefaultReporter.new(color: true)
+]
+
+class MiniTest::Unit::TestCase
+  def before_setup
+    Mongoid.purge!
+  end
+end
