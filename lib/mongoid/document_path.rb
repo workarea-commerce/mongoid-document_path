7485fc7d64b033924fad1479386ff24767cfe489
Ben Crouse <bcrouse@weblinc.com>
2016-10-20 09:36:10 -0400

    Get embedded document working

--- lib/mongoid/document_path.rb
+++ lib/mongoid/document_path.rb
@@ -9,7 +9,12 @@ module Mongoid
       field :type, type: String
       field :document_id, type: String
       field :relation, type: String
+
       embedded_in :document_path, polymorphic: true
+
+      def matches?(document)
+        document.class.name == type && document.id.to_s == document_id
+      end
     end
 
     extend ActiveSupport::Concern
@@ -21,25 +26,25 @@ module Mongoid
         root = path.first.type.constantize.find(path.first.document_id)
         return root if path.one?
 
-        path.reduce(root) do |current, path|
-          relation_match = if document_path_matches?(path, current)
+        path.reduce(root) do |current, node|
+          relation_match = if node.matches?(current)
                              current
                            elsif current.respond_to?(:detect)
-                             current.detect do |model|
-                               document_path_matches?(path, model)
+                             current.detect do |document|
+                               node.matches?(document)
                              end
                            end
 
-          if path['relation'].blank?
+          if node.relation.blank?
             return relation_match
           else
-            relation_match.send(path['relation'])
+            relation_match.send(node.relation)
           end
         end
       end
 
       def document_path_matches?(path, object)
-        object.class.name == path['type'] && object.id == path['id']
+        object.class.name == path['type'] && object.id.to_s == path['document_id']
       end
     end
 
--- test/mongoid/document_path_test.rb
+++ test/mongoid/document_path_test.rb
@@ -4,6 +4,15 @@ class Mongoid::DocumentPathTest < Minitest::Test
   class Order
     include Mongoid::Document
     include Mongoid::DocumentPath
+
+    embeds_one :customer
+  end
+
+  class Customer
+    include Mongoid::Document
+    include Mongoid::DocumentPath
+
+    embedded_in :order
   end
 
   def test_that_it_has_a_version_number
@@ -20,5 +29,23 @@ class Mongoid::DocumentPathTest < Minitest::Test
     assert_nil(document_path.first.relation)
 
     assert_equal(Mongoid::DocumentPath.find(document_path), order)
+  end
+
+  def test_embedded_object_document_paths
+    order = Order.create!
+    customer = order.create_customer
+    document_path = customer.document_path
+
+    assert_equal(document_path.length, 2)
+
+    assert_equal(document_path.first.type, Order.name)
+    assert_equal(document_path.first.document_id, order.id.to_s)
+    assert_equal(document_path.first.relation, 'customer')
+
+    assert_equal(document_path.second.type, Customer.name)
+    assert_equal(document_path.second.document_id, customer.id.to_s)
+    assert_nil(document_path.second.relation)
+
+    assert_equal(Mongoid::DocumentPath.find(document_path), customer)
   end
 end
