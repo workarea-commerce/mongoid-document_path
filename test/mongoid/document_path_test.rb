f933d420867816d93e3566be0e2954a70cdfa48c
Ben Crouse <bcrouse@weblinc.com>
2016-10-20 10:00:10 -0400

    Add test for three-level embedded documents

--- test/mongoid/document_path_test.rb
+++ test/mongoid/document_path_test.rb
@@ -21,6 +21,14 @@ class Mongoid::DocumentPathTest < Minitest::Test
     include Mongoid::DocumentPath
 
     embedded_in :order
+    embeds_one :product
+  end
+
+  class Product
+    include Mongoid::Document
+    include Mongoid::DocumentPath
+
+    embedded_in :item
   end
 
   def test_that_it_has_a_version_number
@@ -74,5 +82,30 @@ class Mongoid::DocumentPathTest < Minitest::Test
     assert_nil(document_path.second.relation)
 
     assert_equal(Mongoid::DocumentPath.find(document_path), item)
+  end
+
+  def test_double_embedded_document_paths
+    order = Order.create!
+    order.items.create!
+    item = order.items.create!
+    product = item.create_product
+    document_path = product.document_path
+
+    puts document_path.inspect
+    assert_equal(document_path.length, 3)
+
+    assert_equal(document_path.first.type, Order.name)
+    assert_equal(document_path.first.document_id, order.id.to_s)
+    assert_equal(document_path.first.relation, 'items')
+
+    assert_equal(document_path.second.type, Item.name)
+    assert_equal(document_path.second.document_id, item.id.to_s)
+    assert_equal(document_path.second.relation, 'product')
+
+    assert_equal(document_path.third.type, Product.name)
+    assert_equal(document_path.third.document_id, product.id.to_s)
+    assert_nil(document_path.third.relation)
+
+    assert_equal(Mongoid::DocumentPath.find(document_path), product)
   end
 end
